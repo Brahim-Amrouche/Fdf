9c98094e8a26d90ccb37e6f9ba5765ebce96664c
Brahim Amrouch <amroucheb4@gmail.com>
2023-02-02 22:34:08 +0100

    started hex parser

--- src/helpers/math.c
+++ src/helpers/math.c
@@ -6,7 +6,7 @@
 /*   By: bamrouch <bamrouch@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 11:58:44 by bamrouch          #+#    #+#             */
-/*   Updated: 2023/02/02 19:35:42 by bamrouch         ###   ########.fr       */
+/*   Updated: 2023/02/02 22:32:54 by bamrouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,39 @@ int	ft_abs(int x)
 	if (x < 0)
 		return -x;
 	return x;
+}
+
+t_boolean	string_is_hex(char *str)
+{
+	size_t i;
+	if (!(str[0] && str[1] && str[0] == '0' && ( str[1] == 'X'  || str[1] == 'x' )))
+		return FALSE;
+	i = 2;
+	while (str[i])
+	{
+		if (!(i < 8 || ft_isdigit(str[i]) || ft_strchr("abcdefABCDEF", str[i])))
+			return FALSE;
+		i++;
+	}
+	return TRUE;
+}
+
+t_boolean	ft_str_is_hex(char *str, int *res)
+{
+	size_t i;
+	int		coef;
+
+	if (!string_is_hex(str))
+		return FALSE;
+	str += 2;
+	i = ft_strlen(str);
+	coef = 1;
+	*res = 0;
+	while (i-- >= 0)
+	{
+		(*res) += coef * 15;//the number in hex
+		coef *= 16;
+	}
 }
 
 t_boolean	ft_str_is_integer(const char *str, int	*res)
